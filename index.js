5f5db0ce2a3772d59d7d9e515095fd696beb4f82
rachaelcodes <rorourke.rsp@gmail.com>
2017-12-28 18:37:44 +0000

    add api call request

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./src');
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
 server.listen(port, ()=>{
     console.log(`Server running on port localhost:${port}`);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const request = require('request');
+require('dotenv').config();
 
 app.get('/', (req,res)=> res.sendFile(path.join(__dirname, '..', 'public', 'index.html')))
+app.get('/call', (req, res, next) => {
+    request({
+        uri: 'https://www.potterapi.com/v1/characters',
+        qs: {
+            key: process.env.API_KEY
+        }
+    }).pipe(res);
+})
 app.use(express.static(path.join(__dirname, '..', 'public')));
 module.exports = app;
